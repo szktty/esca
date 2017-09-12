d7b9547b01e25331270603b92a4eaa01b69937bc
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-09-13 00:36:03 +0900

    rename

--- src/lib_kernel.ml
+++ src/lib_kernel.ml
@@ -1,17 +1,17 @@
 open Core.Std
 
 let init () =
-  Context.Spec.(define "kernel" ~init:true
-                +> typ "unit" Type.unit
-                +> typ "bool" Type.bool
-                +> typ "int" Type.int
-                +> typ "float" Type.float
-                +> typ "string" Type.string
-                +> typ "range" Type.range
-                +> typ "list" Type.list_gen
-                +> typ "option" Type.option_gen
-                +> typ "box" Type.box_gen
-                +> typ "stream" Type.stream
+  Context.Spec.(define "Kernel" ~init:true
+                +> typ "Unit" Type.unit
+                +> typ "Bool" Type.bool
+                +> typ "Int" Type.int
+                +> typ "Float" Type.float
+                +> typ "String" Type.string
+                +> typ "Range" Type.range
+                +> typ "List" Type.list_gen
+                +> typ "Option" Type.option_gen
+                +> typ "Box" Type.box_gen
+                +> typ "Stream" Type.stream
                 +> fun_ "id" Type.Spec.(a @-> a)
                 +> fun_ "print" Type.Spec.(string @-> unit)
                 +> fun_ "printf" Type.Spec.fun_printf
