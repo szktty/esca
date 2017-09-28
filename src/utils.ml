b91d64521589f5e5de941ce56f8f59835668f34c
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-09-29 02:51:21 +0900

    remove an unused function

--- src/utils.ml
+++ src/utils.ml
@@ -16,20 +16,6 @@ let camel_case word =
   |> ignore;
   Buffer.contents buf
 
-let sentencecase word =
-  let buf = Buffer.create (String.length word) in
-  String.foldi word ~init:()
-    ~f:(fun i _ c ->
-        match i with
-        | 0 -> Buffer.add_char buf c
-        | 1 -> Buffer.add_char buf @@ Char.lowercase c
-        | _ ->
-          if Char.is_uppercase c then begin
-            Buffer.add_char buf '_';
-          end;
-          Buffer.add_char buf @@ Char.lowercase c);
-  Buffer.contents buf
-
 module Format = struct
 
   type placeholder =
