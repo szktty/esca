239191712246e61a591ac44b5d47aa408d6ff901
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-09-29 02:50:59 +0900

    naming

--- src/go.ml
+++ src/go.ml
@@ -14,6 +14,23 @@ end
 
 module Name = struct
 
-  let property s = "Esca_" ^ s
+  let prim_pkg = "esca.module"
+
+  let pkg_name ~base mod_name =
+    Printf.sprintf "%s.%s" base (Namepath.to_string ~sep:"." mod_name)
+
+  let import_name mod_name =
+    Printf.sprintf "escaModule_%s" (Namepath.to_string ~sep:"_" mod_name)
+
+  let symbol ~kind name =
+    let prefix = match kind with
+      | `Fun -> "Fun"
+      | `Var -> "Var"
+      | `Type -> "Type"
+    in
+    Printf.sprintf "Esca%s_%s" prefix name
+
+  let property s =
+    Utils.camel_case s
 
 end
