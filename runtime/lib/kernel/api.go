25b712810988d5dbd22c57cab069d5a214b3b326
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-10-02 00:02:28 +0900

    interface

--- runtime/lib/kernel/api.go
+++ runtime/lib/kernel/api.go
@@ -5,7 +5,7 @@ import (
 	"github.com/szktty/esca/runtime/base"
 )
 
-func EscaValue_show(value string) types.Void {
+func EscaValue_show(value interface{}) types.Void {
 	fmt.Println(value)
 	return types.Void{}
 }
